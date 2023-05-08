d3a058603532a68df8c7d7a2a704a79fa14392a8
Rafael Villarroel <rvf0068@gmail.com>
2023-05-08 01:55:28 +0000

    Work only with connect4d graphs and fix problems

--- homsmall.py
+++ homsmall.py
@@ -1,3 +1,6 @@
+"""
+Checks the homotopy type of all graphs up to 7 vertices
+"""
 import networkx as nx
 from pycliques.simplicial import clique_complex
 from pycliques.dominated import pared_graph as p
@@ -5,22 +8,24 @@ from pycliques.cliques import clique_graph as k
 
 
 all_graphs = nx.graph_atlas_g()
-filelog = 'homotopy_types.org'
+RESULTS = 'homotopy_types.org'
 num_all_graphs = len(all_graphs)
 
 
 def main():
-    i = 0
-    with open(filelog, 'a') as f:
-        f.write("| index | order | HT G | HT KG |\n")
-        f.write("|-------+-------+------+-------|\n")
-        for g in all_graphs:
-            pg = p(g)
-            hg = clique_complex(pg).dong_matching()
-            pkg = nx.convert_node_labels_to_integers(p(k(pg)))
-            hkg = clique_complex(pkg).dong_matching()
-            f.write(f"|{i}|{pg.order()}|{hg}|{hkg}|\n")
+    """Main function"""
+    i = -1
+    with open(RESULTS, 'a', encoding="utf8") as the_file:
+        the_file.write("| index | order | HT G | HT KG |\n")
+        the_file.write("|-------+-------+------+-------|\n")
+        for graph in all_graphs:
             i = i+1
+            if nx.is_connected(graph):
+                pared_graph = p(graph)
+                h_g = clique_complex(pared_graph).dong_matching()
+                pkg = nx.convert_node_labels_to_integers(p(k(pared_graph)))
+                hkg = clique_complex(pkg).dong_matching()
+                the_file.write(f"|{i}|{pared_graph.order()}|{h_g}|{hkg}|\n")
 
 
 if __name__ == '__main__':
