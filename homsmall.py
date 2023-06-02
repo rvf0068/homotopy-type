b03744bdc23e237895c6f84663725abb2e44bcd6
Rafael Villarroel <rvf0068@gmail.com>
2023-06-01 20:11:37 -0600

    Return Betti numbers for difficult graphs
    A difficult graph here is a graph that has nonzero homology in two
    dimensions.

--- homsmall.py
+++ homsmall.py
@@ -3,10 +3,15 @@ Checks the homotopy type of all graphs up to 7 vertices
 """
 import argparse
 import random
+import mogutda
 import networkx as nx
 from pycliques.simplicial import clique_complex
 from pycliques.dominated import completely_pared_graph as p
-from pycliques.dominated import has_dominated_vertex, complete_s_collapse, complete_s_collapse_edges
+from pycliques.dominated import (
+    has_dominated_vertex,
+    complete_s_collapse,
+    complete_s_collapse_edges
+    )
 from pycliques.cliques import clique_graph as k
 from pycliques.helly import is_clique_helly
 from pycliques.lists import list_graphs
@@ -48,7 +53,8 @@ def homotopy_type(graph):
     if _read_dong(dong1)[0]:
         return _read_dong(dong1)[1]
     else:
-        c_complex = clique_complex(simplify_ht(graph))
+        s_ht = simplify_ht(graph)
+        c_complex = clique_complex(s_ht)
         dong2 = c_complex.dong_matching()
         if _read_dong(dong2)[0]:
             return _read_dong(dong2)[1]
@@ -59,7 +65,34 @@ def homotopy_type(graph):
                 dong3 = c_complex.dong_matching(order_function=_shuff)
                 if _read_dong(dong3)[0]:
                     return _read_dong(dong3)[1]
-            return (dong1, dong2)
+            return betti_numbers(s_ht)
+
+
+def dimension_sc(s_complex):
+    """Returns the dimension of a simplicial complex"""
+    dims = [len(f) for f in s_complex.face_set]
+    dims.sort()
+    return dims[-1] - 1
+
+
+def betti_numbers(graph):
+    """Computes the betti numbers of the complex of completes of a graph"""
+    def simplify_list(bettis):
+        simplified = bettis
+        while len(simplified) > 0 and simplified[-1] == 0:
+            del simplified[-1]
+        return simplified
+    the_simplices = [tuple(c) for c in nx.find_cliques(graph)]
+    the_complex = mogutda.SimplicialComplex(simplices=the_simplices)
+    dim = dimension_sc(the_complex)
+    numbers = [the_complex.betti_number(i) for i in range(dim+1)]
+    numbers[0] = numbers[0] - 1  # reduced betti number
+    return simplify_list(numbers)
+
+
+def max_degree(graph):
+    degrees = dict(graph.degree())
+    return max(degrees.values())
 
 
 def main():
@@ -78,7 +111,8 @@ def main():
         results = f"homotopy_types_{args.order}.org"
 
         def conditions(graph):
-            return not has_dominated_vertex(graph)
+            return (not has_dominated_vertex(graph)
+                    and max_degree(graph) >= 6) 
     i = -1
     with open(results, 'a', encoding="utf8") as the_file:
         the_file.write("| index | order | Helly | K Helly | HT G | HT KG |\n")
@@ -95,7 +129,15 @@ def main():
                     is_helly = is_clique_helly(graph)
                     is_k_helly = is_clique_helly(pkg)
                     if not (is_helly and ("S^{1}" in h_g or "S^{1}" in hkg)):
-                        the_file.write(f"|{i}|{p_g.order()}|{is_helly}|{is_k_helly}|{h_g}|{hkg}|\n")
+                        the_file.write("|" + str(i) +
+                                       "|" + str(p_g.order()) +
+                                       "|" + str(is_helly) +
+                                       "|" + str(is_k_helly) +
+                                       "|" + str(h_g) +
+                                       "|" + str(hkg) +
+                                       "|\n")
+                else:
+                    the_file.write("|Clique graph has at least 23 vertices||||||\n")
 
 
 if __name__ == '__main__':
